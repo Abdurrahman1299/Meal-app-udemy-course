e622054890a2284942b6181fdecbe6b1705965c8
Abdurrahman1299 <abdurrahmanms09@gmail.com>
2023-03-11 19:32:15 +0200

    mar-11

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import CategoriesScreen from "./screens/CategoriesScreen";
-import CategoriesOver from "./screens/CategoriesOver";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import CategoriesScreen from "./screens/CategoriesScreen";
+import CategoriesOver from "./screens/CategoriesOver";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import Favorites from "./screens/Favorites";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          headerTitleAlign: "center",
+        }}
+      />
+      <Drawer.Screen name="Favorites" component={Favorites} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -19,7 +38,13 @@ export default function App() {
             headerTitleAlign: "center",
           }}
         >
-          <Stack.Screen name="Categories" component={CategoriesScreen} />
+          <Stack.Screen
+            name="Drawer"
+            component={DrawerNavigator}
+            options={{
+              headerShown: false,
+            }}
+          />
           <Stack.Screen name="MealsOverView" component={CategoriesOver} />
           <Stack.Screen name="MealDetailScreen" component={MealDetailScreen} />
         </Stack.Navigator>
